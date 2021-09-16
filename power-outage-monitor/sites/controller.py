1a5c82a386539eae6988e05f5b97eb34df5a5ef4
Jonny Le <jonny.le@computacenter.com>
2021-09-16 17:56:24 +0000

    ignore case site name and added county

--- power-outage-monitor/sites/controller.py
+++ power-outage-monitor/sites/controller.py
@@ -6,16 +6,20 @@ import sqlite3
 import yaml
 from loguru import logger
 
+
 with open(os.path.join(os.path.dirname(__file__), os.pardir, "config.yaml")) as config_stream:
     config = yaml.safe_load(config_stream)
+
 
 @logger.catch
 def init_db():
     con = sqlite3.connect(config["sqlite3"]["dbName"])
+    con.execute("DROP TABLE sites")
     con.execute("""CREATE TABLE sites
                     (name TEXT PRIMARY KEY,
                      street TEXT,
                      city TEXT,
+                     county TEXT,
                      state TEXT,
                      longitude REAL,
                      latitude REAL)""")
@@ -31,9 +35,9 @@ def populate_site(file_name):
             reader = csv.DictReader(csv_file)
             site_list = []
             for row in reader:
-                site_list.append((row["name"], row["street"], row["city"], row["state"], row["longitude"], row["latitude"]))
+                site_list.append((row["name"], row["street"], row["city"], row["county"], row["state"], row["longitude"], row["latitude"]))
         con = sqlite3.connect(config["sqlite3"]["dbName"])
-        con.executemany("INSERT INTO sites(name, street, city, state, longitude, latitude) VALUES (?, ?, ?, ?, ?, ?)", site_list)
+        con.executemany("INSERT INTO sites(name, street, city, county, state, longitude, latitude) VALUES (?, ?, ?, ?, ?, ?, ?)", site_list)
         con.commit()
         con.close()
         logger.info("Successfully populated sites.")
@@ -42,37 +46,23 @@ def populate_site(file_name):
             data = json.load(json_file)
             site_list = []
             for site in data:
-                site_list.append((site["name"], site["street"], site["city"], site["state"], site["longitude"], site["latitude"]))
+                site_list.append((site["siteName"], site["street"], site["city"], site["county"], site["state"], site["longitude"], site["latitude"]))
         con = sqlite3.connect(config["sqlite3"]["dbName"])
-        con.executemany("INSERT INTO sites(name, street, city, state, longitude, latitude) VALUES (?, ?, ?, ?, ?, ?)", site_list)
+        con.executemany("INSERT INTO sites(name, street, city, county, state, longitude, latitude) VALUES (?, ?, ?, ?, ?, ?, ?)", site_list)
         con.commit()
         con.close()
         logger.info("Successfully populated sites.")
     else:
-        logger.error("File not yet supported.")
+        logger.error("File not supported.")
 
 
 @logger.catch
-def query_to_site(query):
-    if query:
-        return {
-            "siteName": query[0],
-            "street": query[1],
-            "city": query[2],
-            "state": query[3],
-            "longitude": query[4],
-            "latitude": query[5]
-        }
-    return None
-
-
-@logger.catch
-def add_site(site_name, street, city, state, longitude, latitude):
+def add_site(site_name, street, city, county, state, longitude, latitude):
     con = sqlite3.connect(config["sqlite3"]["dbName"])
     logger.info("Adding site " + site_name + " to db.")
     try:
         with con:
-            con.execute("INSERT INTO sites(name, street, city, state, longitude, latitude) VALUES (?, ?, ?, ?, ?, ?)", (site_name, street, city, state, longitude, latitude))
+            con.execute("INSERT INTO sites(name, street, city, county, state, longitude, latitude) VALUES (?, ?, ?, ?, ?, ?, ?)", (site_name, street, city, county, state, longitude, latitude))
     except sqlite3.IntegrityError:
         logger.error("Site '" + site_name + "' already exists.")
         return None
@@ -86,23 +76,28 @@ def add_site(site_name, street, city, state, longitude, latitude):
 def get_site(site_name):
     con = sqlite3.connect(config["sqlite3"]["dbName"])
     logger.info("Getting site " + site_name + " from db.")
-    query = con.execute("SELECT * FROM sites WHERE name=?", (site_name,)).fetchone()
+    con.row_factory = sqlite3.Row
+    query = dict(con.execute("SELECT * FROM sites WHERE name=? COLLATE NOCASE", (site_name,)).fetchone())
     con.close()
-    return query_to_site(query)
+    query["siteName"] = query.pop("name")
+    return query
 
 
 @logger.catch
 def get_all():
     con = sqlite3.connect(config["sqlite3"]["dbName"])
     logger.info("Getting all sites from db.")
-    query = con.execute("SELECT * FROM sites").fetchall()
+    con.row_factory = sqlite3.Row
+    query = [dict(row) for row in con.execute("SELECT * FROM sites").fetchall()]
     con.close()
-    return [query_to_site(row) for row in query]
+    for row in query:
+        row["siteName"] = row.pop("name") 
+    return query
 
 
 def main():
     init_db()
-    populate_site("site.json")
+    populate_site(os.path.join(os.path.dirname(__file__), "site.json"))
 
 
 if __name__ == "__main__":
