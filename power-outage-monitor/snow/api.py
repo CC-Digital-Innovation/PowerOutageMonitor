6dac582656348c12034c3f0092f8fd69b05d320e
Jonny Le <jonnyle2@gmail.com>
2022-08-24 12:24:42 -0500

    add snow connection

---
+++ power-outage-monitor/snow/api.py
@@ -0,0 +1,63 @@
+import pysnow
+import requests
+
+class SnowApi:
+    def __init__(self, instance, username, password, limit=10000, offset=0, display_value=False):
+        self.client = pysnow.Client(instance=instance, user=username, password=password)
+        self.client.parameters.limit = limit
+        self.client.parameters.offset = offset
+        self.client.parameters.display_value = display_value
+        self.username = username
+        self.password = password
+
+    def get_record(self, link):
+        response = requests.get(link, auth=(self.username, self.password))
+        response.raise_for_status()
+        return response.json()['result']
+
+    def get_choice_labels(self, choice):
+        choice_table = self.client.resource(api_path='/table/sys_choice')
+        response = choice_table.get(query={'element': choice})
+        return sorted({category['label'] for category in response.all()})
+
+    def get_cis_filtered_by(self, filters):
+        ci_table = self.client.resource(api_path='/table/cmdb_ci')
+
+        copy_filters = filters.copy()
+        try:
+            first_k, first_v = copy_filters.popitem()
+        except KeyError:
+            return ci_table.get().all()
+        # first query
+        query = pysnow.QueryBuilder().field(first_k)
+        if v[0]:
+            query.equals(first_v[0])
+        else:
+            query.is_empty()
+        for i in range(1, len(first_v)):
+            query.OR().field(first_k)
+            if v[i]:
+                query.equals(first_v[i])
+            else:
+                query.is_empty()
+        # subsequent queries appended by AND()
+        for k, v in copy_filters.items():
+            query.AND().field(k)
+            if v[0]:
+                query.equals(v[0])
+            else:
+                query.is_empty()
+            for i in range(1, len(v)):
+                query.OR().field(k)
+                if v[i]:
+                    query.equals(v[i])
+                else:
+                    query.is_empty()
+        response = ci_table.get(query=query)
+        return response.all()
+
+    def set_field(self, sys_id, name, value):
+        update = {name: value}
+        ci_table = self.client.resource(api_path='/table/cmdb_ci')
+        response = ci_table.update(query={'sys_id': sys_id}, payload=update)
+        return response[name] == value
